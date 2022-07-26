884cac8b3b761e2186e797dce9908ea41b39a60f
Igor <munkieryder@gmail.com>
2022-07-26 20:23:08 +0200

    fetch listings from firestore

---
+++ src/pages/Category.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Loader from "../components/Loader";
+
+function Category(props) {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // get reference
+        const listingsRef = collection(db, "listings");
+
+        // create a query,  param is from App.js (categoryName)
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        // execute query
+        const querySnap = await getDocs(q);
+
+        const listings = [];
+
+        querySnap.forEach((doc) => {
+          // console.log(doc.data());
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch (e) {
+        toast.error(`Couldn't fetch Listings`);
+      }
+    };
+
+    fetchListings();
+  }, []);
+
+  console.log("listings", listings);
+  return <div></div>;
+}
+
+export default Category;
